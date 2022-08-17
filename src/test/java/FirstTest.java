2d807cf1e81562343c7efb306707801aa5832dad
Jarin <jarinkazi06@gmail.com>
2022-08-17 18:47:30 -0400

    adding a new project

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,17 @@
+import browser.Browser;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class FirstTest {
+
+    @Test
+    public void test1() throws IOException {
+        Browser.setDriver();
+    }
+
+
+
+
+
+}
