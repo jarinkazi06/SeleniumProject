24dd07d10e4fc0f71db5d64624438a14d54144c0
Jarin <jarinkazi06@gmail.com>
2022-08-17 20:14:36 -0400

    deleting a line

--- src/test/java/alltests/HomePageTest.java
+++ src/test/java/alltests/HomePageTest.java
@@ -24,7 +24,6 @@ public class HomePageTest {
 
     @Test
     public void test1(){
-        System.out.println("This is a test");
         System.out.println("Testing git");
         System.out.println("Making another");
     }
